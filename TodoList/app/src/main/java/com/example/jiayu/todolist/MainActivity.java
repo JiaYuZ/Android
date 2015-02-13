5d4dff29c566aa1263051a08c5344ee3d1421a06
Jiayu <jiayuzeng0000@gmail.com>
2015-02-13 15:54:09 -0800

    TodoList
    Nexus 5 API 21

---
+++ TodoList/app/src/main/java/com/example/jiayu/todolist/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.jiayu.todolist;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends ActionBarActivity {
+    private ListView listView;
+    private ArrayAdapter<String> adapter;
+    private EditText editText;
+    private Button button;
+    private ArrayList<String> items;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        listView = (ListView) findViewById(R.id.list_view);
+        items = new ArrayList<String>();
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
+        removeItem(listView);
+    }
+
+    public void onAddItem(View v){
+        editText = (EditText) findViewById(R.id.edit_new_item);
+        String itemText = editText.getText().toString();
+        adapter.add(itemText);
+        editText.setText("");
+    }
+
+    public void removeItem(ListView v){
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                items.remove(position);
+                adapter.notifyDataSetChanged();
+                return false;
+
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
